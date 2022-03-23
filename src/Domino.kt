924ac3fdfb0628c5f0da3d6a0f4781b98ec64e8a
iamsrvdas <sourav@xoxoday.com>
2022-03-24 01:47:49 +0530

    Domino problem

---
+++ src/Domino.kt
@@ -0,0 +1,12 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+
+    val sc = Scanner (System.`in`)
+    val m = sc.nextInt()
+    val n = sc.nextInt()
+    val p = m*n
+    val d: Int = p/2
+
+    println(d)
+}
